91a6f5d78f958211440e35327038f7fb384aced3
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-03-19 17:55:51 -0300

    Iniciando com blocos try, catch and finally

---
+++ StartingExeptions/StartingExeptions/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ExceptionsMod
+{
+    public class ExceptionsClass 
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+                int Result = N1 / N2;
+                Console.WriteLine(Result);
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Error! You can't divide a number by zero! please do not do that.");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("You can't enter on a alphabetic letter, only integers numbers! ");
+            }
+
+        }
+    }
+}
